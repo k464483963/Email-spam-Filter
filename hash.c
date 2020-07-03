83ae55bb6acdb3190a14dffad4366cf766df730c
Hanjie Zhan <cs30s120cg@ieng6-201.ucsd.edu>
2020-07-03 16:01:05 -0700

    second modi

--- hash.c
+++ hash.c
@@ -7,11 +7,20 @@
  */
  
 #include <stdio.h>
+#include <string.h>
 #include "pa1.h"
+#define INCREMENT 2
 
 
- unsigned int hash (char * str){
+ unsigned int hash (char * str){	 
 	unsigned int hashVal = HASH_START_VAL;
-
+	for(int i=0;i<strlen(str);i=i+INCREMENT){
+		hashVal=(hashVal*HASH_PRIME)+str[i];
+	}
+	for(int i=1;i<strlen(str);i=i+INCREMENT){	
+		hashVal=(hashVal*HASH_PRIME)+str[i];
+	}
+	
+	return hashVal;
 
 }

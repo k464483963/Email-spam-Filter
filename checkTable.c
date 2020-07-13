54748f2e64dd5d9b33d64aa3ecfd822438e39f5b
Hanjie Zhan <cs30s120cg@ieng6-201.ucsd.edu>
2020-07-13 00:35:40 -0700

    “finaltest”

--- checkTable.c
+++ checkTable.c
@@ -1,27 +1,45 @@
 /*
- * Filename: hash.c
+ * Filename: checkTable.c
  * Author: HanJie Zhan
  * UserId: cs30s120cg
  * Date:07/05/2020
  * Sources of help: none.
  */
- 
+
 #include <stdio.h>
 #include <string.h>
 #include "pa1.h"
 
+/*Function Name: checkTable.c
+ *Function Prototype:int checkTable(char * str, linkedListNode_t ** hashtbl);
+ *Description: this function would check if the linklist exist in the hash 
+ *table.
+ *Parameters: char string from the user. hash table double pointer 
+ *Side Effects: None
+ *Error Conditions: None
+ *return value: return zero if the linklist exist.
+ */
 int checkTable(char * str, linkedListNode_t ** hashtbl){
+	//return the linklist pointer to newPointer
 	linkedListNode_t *newPointer = retrieveLinkedList(hashtbl,str);
 
-	if(newPointer==NULL){
+	//if the linklist not exist return zero.
+	if(newPointer==NULL) {
 		return 0;
 	}
-	
-	while(newPointer!=NULL){
+
+	//chec if the value is exist in the linklist
+	while(newPointer != NULL) {
+
+		//compare the value with Node
 		if(strcmp((*newPointer).value,str)==0){
 			return 1;
 		}
+
+		//if not found value go to next Node
 		newPointer = newPointer->next;	
+
 	}	
+
 	return 0;
 }
--- spamFilter.c
+++ spamFilter.c
@@ -1,47 +1,78 @@
 /*
- * Filename: hash.c
+ * Filename: spamFilter.c
  * Author: HanJie Zhan
  * UserId: cs30s120cg
  * Date:07/05/2020
- * Sources of help: none.
+ * Sources of help: tutors.
  */
 
 #include <stdio.h>
 #include <string.h>
 #include "pa1.h"
 #include "pa1Strings.h"
+#define  TWO_COMMAND 2
+#define  THREE_COMMAND 3
+
+/*Function Name: spamFilter.c
+ *Function Prototype:int main(int argc, char *argv[] );
+ *Description:  this function is main function, first open the files and 
+ *do error input checking.then check if the email is exist in the files.
+ *Parameters: the number of argument int argc, the array of argument char
+ *argv[];
+ *Side Effects: None
+ *Error Conditions: if open the wrong files and the email can not read.
+ *return value: return zero if the files open seccessful.
+ */
 
 int main(int argc, char *argv[] ){
-	if(argc>3||argc<2){
+	//check if there are more than two command line
+	if(argc > THREE_COMMAND || argc < TWO_COMMAND) {
+
 		fprintf(stderr,INVALID_ARGS );
 
 		fprintf(stderr,SHORT_USAGE);
 
-
 		return 1;
 	}
 
-	FILE * fw;
-	if(argc==2){
+	FILE * fw;//file type pointer
+
+	//if the command line have one then print messeage
+	if(argc == TWO_COMMAND){
+
 		printf(LONG_USAGE);
+
 		return 0;
 	}
-	if(argc==3){
-		fw=fopen(argv[2],"rb");
+
+	//if the command line have two then print message
+	if(argc == THREE_COMMAND){
+
+		//open the file then read the contest inside file
+		fw=fopen(argv[TWO_COMMAND],"rb");
+
 	}
+
+	//if the files can not open.
 	if(fw == NULL){
+
 		perror(FILTER_ERR);
+
 		fprintf(stderr,SHORT_USAGE);
+
 		return 1;
 	}
 
+	//create a newLinklist hash table
 	linkedListNode_t ** newLinklist = newLinkedListArray(DEFAULT_SIZE);
 
+	//get the contest from the files.
 	populateTable(newLinklist,fw);
 
-
+	//display the user interface 
 	launchUserQuery(newLinklist);
 
+	//clearup the linlist
 	cleanup(newLinklist);
 
 	fclose(fw);
